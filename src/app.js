c3947ed923b15e88b4a280935de4624e623871f5
gabrielmussolini984 <gabrielmussolini@hotmail.com>
2020-02-11 11:53:33 -0300

    delay

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import dotenv from 'dotenv';
 import { resolve } from 'path';
-import delay from 'express-delay';
-
 
 dotenv.config();
 
@@ -40,7 +38,6 @@ class App {
   middleware() {
     this.app.use(cors(corsOption));
     this.app.use(helmet());
-    this.app.use(delay(2000));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
